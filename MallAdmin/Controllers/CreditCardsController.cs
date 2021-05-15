f05b3a1656811fc7261891a8c050d5d1e7fddf8e
NGNhan719 <67472096+NGNhan719@users.noreply.github.com>
2021-05-15 08:56:41 +0700

    MallAdmin: add table and form

---
+++ MallAdmin/Controllers/CreditCardsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MumbaiMallLibrary;
+
+namespace MallAdmin.Controllers
+{
+    public class CreditCardsController : Controller
+    {
+        private malldbEntities db = new malldbEntities();
+
+        // GET: CreditCards
+        public ActionResult Index()
+        {
+            return View(db.CreditCards.ToList());
+        }
+
+        // GET: CreditCards/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CreditCard creditCard = db.CreditCards.Find(id);
+            if (creditCard == null)
+            {
+                return HttpNotFound();
+            }
+            return View(creditCard);
+        }
+
+        // GET: CreditCards/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CreditCards/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,CardNumber,CardOwner,CardType,ExpiredDate,CVC")] CreditCard creditCard)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CreditCards.Add(creditCard);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(creditCard);
+        }
+
+        // GET: CreditCards/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CreditCard creditCard = db.CreditCards.Find(id);
+            if (creditCard == null)
+            {
+                return HttpNotFound();
+            }
+            return View(creditCard);
+        }
+
+        // POST: CreditCards/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,CardNumber,CardOwner,CardType,ExpiredDate,CVC")] CreditCard creditCard)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(creditCard).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(creditCard);
+        }
+
+        // GET: CreditCards/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CreditCard creditCard = db.CreditCards.Find(id);
+            if (creditCard == null)
+            {
+                return HttpNotFound();
+            }
+            return View(creditCard);
+        }
+
+        // POST: CreditCards/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CreditCard creditCard = db.CreditCards.Find(id);
+            db.CreditCards.Remove(creditCard);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
